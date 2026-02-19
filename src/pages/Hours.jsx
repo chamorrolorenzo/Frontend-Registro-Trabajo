ed5510e6b644436f414dd09bb8d4beb2713ca308
lorenzochamorro <chamorrolorenzo@hotmail.com>
2026-02-19 18:14:47 -0300

    use Hours.css import

--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import SimpleMap from "../components/SimpleMap";
-import "../styles/hours.css";
+import "../styles/Hours.css";
 
 function Hours() {
   return (
