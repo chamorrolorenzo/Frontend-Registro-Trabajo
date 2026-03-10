2e978e1e11b2c9a08ea9f6172e3544c6ba1b4ebe
lorenzochamorro <chamorrolorenzo@hotmail.com>
2026-03-10 20:03:24 -0300

    Add PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 import "./styles/toast.css";
-
 import "leaflet/dist/leaflet.css";
+
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Toaster
-  position="top-center"
-  containerStyle={{
-    top: "50%",
-    transform: "translateY(-50%)",
-  }}
-/>
+      position="top-center"
+      containerStyle={{
+        top: "50%",
+        transform: "translateY(-50%)",
+      }}
+    />
   </React.StrictMode>
 );
-s
